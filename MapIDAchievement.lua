1d1a16dde0087e9d13ca4cd084edf33a101a96ea
husandro <husandro@qq.com>
2025-10-30 16:02:22 +0100

    Update MapIDAchievement.lua

--- MapIDAchievement.lua
+++ MapIDAchievement.lua
@@ -2,7 +2,7 @@
 {Instance_ID,ID}
 https://wago.tools/db2/Achievement?locale=zhCN
 ]]
-local tab=
+local tab
 
 EventRegistry:RegisterFrameEventAndCallback("ADDON_LOADED", function(owner, arg1)
      if arg1~='WoWTools_Chinese_Scanner' then
@@ -14,7 +14,7 @@ EventRegistry:RegisterFrameEventAndCallback("ADDON_LOADED", function(owner, arg1
        for _, data in pairs(tab) do
             local mapID= data[1] or 0
             local achievementID= data[2] or 0
-            if mapID>0 and achievementID>0 then
+            if mapID>10 and achievementID>0 then
 
                 if not select(10, GetAchievementInfo(achievementID))~= 136243 then
                     WoWTools_SC_MapIDAchievements[mapID]= WoWTools_SC_MapIDAchievements[mapID] or {}
